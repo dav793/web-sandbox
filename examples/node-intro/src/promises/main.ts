4a5f2967be3fc9e67dd30e8dad9d81b3f54b3a81
David Vargas <dav793@gmail.com>
2024-05-19 18:53:50 -0600

    Agregar ejemplos de promises

--- examples/node-intro/src/promises/main.ts
+++ examples/node-intro/src/promises/main.ts
@@ -28,6 +28,15 @@
 //     resolve(promise);
 // });
 
+// Promises encadenados
+// const miPromise = new Promise((resolve, reject) => {
+//     resolve(5);
+// });
+
+// miPromise
+//     .then((x: number) => x + 5)
+//     .then(x => console.log(x));
+
 // const miPromiseConError = Promise.reject( new Error('error de X tipo') );
 // miPromiseConError.then(
 //     result => console.log(`Exito: ${result}`),
@@ -53,6 +62,40 @@
 // console.log('estoy haciendo cosas');
 
 // Fetch API data
-fetch('http://example.com/movies.json')
-    .then( response => response.json() )
-    .then( json => console.log(json) );
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//     .then( response => response.json() )
+//     .then( json => console.log(json) );
+
+// const jsonObj = `
+//     {
+//         "miArreglo": [
+//             1, 
+//             2, 
+//             3
+//         ]
+//     }
+// `;
+// const obj = JSON.parse(jsonObj);
+// console.log( obj.miArreglo[0] );
+
+// const jsonObj2 = JSON.stringify(obj);
+// console.log(jsonObj2);
+
+// Diferencia entre async/await y promises normales
+// const miPromise = new Promise((resolve, reject) => {
+//     resolve('A');
+// });
+
+// promise normal:
+//  - primero imprime 'B' y luego 'A'
+// miPromise.then(x => console.log(x));
+// console.log('B');
+
+// promise con async/await:
+//  - primero imprime 'A' y luego 'B'
+// async function exec() {
+//     const res = await miPromise;
+//     console.log(res);
+//     console.log('B');
+// }
+// exec();
